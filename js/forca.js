b16b583aba53bb72a8f53d6a822a11b0a979c5c6
emsjenifer <emsjenifer@gmail.com>
2023-08-15 14:41:25 -0300

    (feat) Add functions to verify if user win or loose and reset the game

--- js/forca.js
+++ js/forca.js
@@ -1,13 +1,13 @@
 const listaPalavras = [
   {
-    dica: "Animal",
-    palavras: ["Macaco", "Cachorro", "Orangotango", "Rinoceronte"],
+    dica: 'Animal',
+    palavras: ['MACACO', 'CACHORRO', 'ORANGOTANGO', 'RINOCERONTE'],
   },
-  { dica: "Fruta", palavras: ["Abacate", "Tomate", "Graviola", "Cupuacu"] },
+  { dica: 'Fruta', palavras: ['ABACATE', 'TOMATE', 'GRAVIOLA', 'BANANA'] },
 ];
 
 let dica;
-let palavraSorteada = "";
+let palavraSorteada = '';
 let maxErros = 6;
 let erros = 0;
 let chute = [];
@@ -16,18 +16,16 @@ let statusDaPalavra = null;
 function sorteiaPalavra() {
   const indiceObjeto = Math.floor(Math.random() * listaPalavras.length);
   dica = listaPalavras[indiceObjeto].dica;
-  console.log(dica);
 
   const indicePalavra = Math.floor(
     Math.random() * listaPalavras[indiceObjeto].palavras.length
   );
   palavraSorteada = listaPalavras[indiceObjeto].palavras[indicePalavra];
-  console.log(palavraSorteada);
 }
 
 function gerarBotoes() {
-  let botoesHTML = "abcdefghijklmnopqrstuvwxyz"
-    .split("")
+  let botoesHTML = 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
     .map(
       (letra) =>
         `
@@ -46,50 +44,77 @@ function gerarBotoes() {
       </button>
     `
     )
-    .join("");
+    .join('');
 
-  document.getElementById("teclado").innerHTML = botoesHTML;
+  document.getElementById('teclado').innerHTML = botoesHTML;
 }
 
-document.getElementById("maxErros").innerHTML = maxErros;
+document.getElementById('maxErros').innerHTML = maxErros;
 
 function advinhaPalavra() {
   statusDaPalavra = letras
     .map((letra) => (chute.indexOf(letra) >= 0 ? letra : " _ "))
     .join("");
 
-  document.getElementById("palavraDestacada").innerHTML = statusDaPalavra;
+  document.getElementById('palavraDestacada').innerHTML = statusDaPalavra;
 }
 
 function verificaChute(letraChutada) {
   chute.push(letraChutada.toUpperCase());
-  document.getElementById(letraChutada).setAttribute("disabled", true);
+  document.getElementById(letraChutada).setAttribute('disabled', true);
 
   if (letras.includes(letraChutada.toUpperCase())) {
     advinhaPalavra();
+    verificaSeGanhou();
   } else {
     adicionaErro();
+    verificaSePerdeu();
+  }
+}
+
+function verificaSeGanhou() {
+  if(statusDaPalavra === palavraSorteada) {
+    document.getElementById('teclado').innerHTML = 'Você ganhou!'
+  }
+}
+
+function verificaSePerdeu() {
+  if(erros === maxErros) {
+    document.getElementById('palavraDestacada').innerHTML = `A resposta era: ${palavraSorteada}`;
+    document.getElementById('teclado').innerHTML = 'Você perdeu!'
   }
 }
 
 function adicionaErro() {
   erros++;
 
-  document.getElementById("erros").innerText = erros;
+  document.getElementById('erros').innerText = erros;
 
   document
-    .getElementById("imagemForca")
-    .setAttribute("src", `imagens/forca${erros}.png`);
+    .getElementById('imagemForca')
+    .setAttribute('src', `imagens/forca${erros}.png`);
 }
 
 function escreveDica(dica) {
-  let paragrafoDica = document.getElementById("dica");
+  let paragrafoDica = document.getElementById('dica');
   paragrafoDica.innerText = `A dica é: ${dica}`;
+}
+
+function limpar() {
+  erros = 0;
+  chute = [];
+  document.getElementById('imagemForca').src = './imagens/forca0.png';
+  document.getElementById('erros').innerText = erros;
+
+  sorteiaPalavra();
+  gerarBotoes();
+  advinhaPalavra();
+  escreveDica(dica);
 }
 
 sorteiaPalavra();
 let palavraMaiuscula = palavraSorteada.toUpperCase();
-let letras = palavraMaiuscula.split("");
+let letras = palavraMaiuscula.split('');
 gerarBotoes();
 advinhaPalavra();
 escreveDica(dica);
